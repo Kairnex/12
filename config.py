632dcbb2046734fceeb39115064b1cad9cc554cd
Kairnex <codexkairnex@gmail.com>
2025-06-12 15:40:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://codexkairnex:gm6xSxXfRkusMIug@cluster0.bplk1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7758785176:AAGqM9GnLngx0Uv5zKg13Pr0UsBEoHTV_S8")
+API_ID = int(os.getenv("API_ID", "21189715"))
+API_HASH = os.getenv("API_HASH", "988a9111105fd2f0c5e21c2c2449edfd")
 
-SUDO_USERS = [123456789, 987654321]  # Replace with real user IDs
+SUDO_USERS = [7416620797, 6999372290]  # Replace with real user IDs
